40d2bc63f8f6d0108d92dd571146196d634e1eb4
EliasB <eliasbgilbert@gmail.com>
2019-07-25 21:58:34 -0700

    New user connection to PHP / MySQL implemented

--- app/src/main/java/com/ClassNote/blank_app/ConnectMySQL.java
+++ app/src/main/java/com/ClassNote/blank_app/ConnectMySQL.java
@@ -52,4 +52,48 @@ public class ConnectMySQL{
         //d.execute(username, password);
         return d.doInBackground(username, password);
     }
+
+    public String newUser(String username, String password, String name, String email) {
+
+        class DownloadJSON extends AsyncTask<String, Void, String> {
+
+            public DownloadJSON() {
+                super();
+            }
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected String doInBackground(String... strings) {
+                try{
+                    URL url = new URL("http://classnoteutil.000webhostapp.com/new_user.php?username="+strings[0]+"&password="+strings[1]+"&name="+strings[2]+"&email="+strings[3]);
+                    //System.out.println(url);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    //System.out.println(conn);
+                    BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    //System.out.println(rd);
+                    String response = rd.readLine();
+                    return response;
+                } catch(Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                try {
+                    System.out.println(s);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        DownloadJSON d = new DownloadJSON();
+        return d.doInBackground(username, password, name, email);
+    }
 }
